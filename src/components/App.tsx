1b8895de249523f4583487ece9bdf3c72132f245
Dustin Hurtt <dustin_hurtt@outlook.com>
2022-08-30 14:22:43 -0400

    first commit

---
+++ src/components/App.tsx
@@ -0,0 +1,35 @@
+import * as esbuild from 'esbuild-wasm'
+import { useState, useEffect } from "react";
+
+const App = () => {
+    const [input, setInput] = useState('')
+    const [code, setCode] = useState('')
+
+    const startService = async () => {
+        const service = await esbuild.startService({
+            worker: true,
+            wasmURL: '/esbuild.wasm'
+        });
+        console.log(service)
+    };
+
+    useEffect(() => {
+        startService()
+    }, [])
+
+    const onClick = () => {
+        console.log(input)
+    }
+
+    return (
+            <div>
+                <textarea value={input} onChange={e => setInput(e.target.value)} aria-label="textarea"></textarea>
+                <div>
+                    <button onClick={onClick}>Submit</button>
+                </div>
+                <pre>{code}</pre>
+            </div>
+        )
+};
+
+export default App
