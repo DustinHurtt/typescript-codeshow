818fb15fa767d40849129e247720fcdab61d4d20
Dustin Hurtt <dustin_hurtt@outlook.com>
2022-09-05 15:57:14 -0400

    added css and code highlighting

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,7 +1,11 @@
+import './code-editor.css';
+import './syntax.css'
 import { useRef } from 'react';
 import MonacoEditor, { EditorDidMount } from '@monaco-editor/react';
-import prettier from 'prettier'
-import parser from 'prettier/parser-babel'
+import prettier from 'prettier';
+import parser from 'prettier/parser-babel';
+import codeShift from 'jscodeshift';
+import Highlighter from 'monaco-jsx-highlighter';
 
 interface CodeEditorProps {
     initialValue: string;
@@ -17,6 +21,19 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ onChange, initialValue }) => {
             onChange(getValue());
         });        
         // monacoEditor.getModel()?.updateOptions({ tabSize: 2})
+
+        const highlighter = new Highlighter(
+            // @ts-ignore
+            window.monaco,
+            codeShift,
+            monacoEditor
+        );
+        highlighter.highLightOnDidChangeModelContent(
+            () => {},
+            () => {},
+            undefined,
+            () => {}
+        );
     };
 
     const onFormatClick = () => {
@@ -29,15 +46,15 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ onChange, initialValue }) => {
             plugins: [parser],
             semi: true,
             singleQuote: true
-        })
+        }).replace(/\n$/, '');
 
         //set the formatted value back in the editor
         editorRef.current.setValue(formatted)
     };
 
     return (
-        <div>
-            <button onClick={onFormatClick}>Format</button>
+        <div className="editor-wrapper">
+            <button className="button button-format is-primary is-small" onClick={onFormatClick}>Format</button>
             <MonacoEditor
                 editorDidMount={onEditorDidMount}
                 value={initialValue}
