837a65f0895fe16a3d6f7fdfb425752a89cadd9b
Dustin Hurtt <dustin_hurtt@outlook.com>
2022-09-05 21:52:34 -0400

    added Resizable component, will need more configuring

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -1,3 +1,19 @@
+import './resizable.css'
 import { ResizableBox } from "react-resizable";
 
-const Re
+interface ResizableProps {
+    direction: "horizontal" | "vertical";
+    children?: React.ReactNode;
+}
+
+const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
+    return <ResizableBox 
+                height={300}
+                width={300}
+                resizeHandles={['s']}
+            >
+                {children}
+            </ResizableBox>;
+};
+
+export default Resizable 
