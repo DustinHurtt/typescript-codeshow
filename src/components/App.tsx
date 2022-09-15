790b6ca7a48f4ec437660ff133583f526c8c2bda
Dustin Hurtt <dustin_hurtt@outlook.com>
2022-09-15 17:04:50 -0400

    continued work on redux

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,6 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css'
+import { Provider } from 'react-redux';
+import { store } from '../state';
 import CodeCell from './code-cell';
 import TextEditor from './text-editor';
 
@@ -6,10 +8,12 @@ import TextEditor from './text-editor';
 const App = () => {
 
     return (
+        <Provider store={store}>
             <div>
                 {/* <CodeCell /> */}
                 <TextEditor />
             </div>
+        </Provider>
         )
 };
 
