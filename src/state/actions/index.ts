19ad2a5646984e0eec30d3562094bbd8cd268c01
Dustin Hurtt <dustin_hurtt@outlook.com>
2022-09-15 11:59:40 -0400

    beginning of redux design

---
+++ src/state/actions/index.ts
@@ -0,0 +1,18 @@
+import { ActionType } from "../action-types";
+
+interface MoveCellAction {
+    type: ActionType.MOVE_CELL;
+    
+}
+
+interface DeleteCellAction {
+    type: ActionType.DELETE_CELL;
+}
+
+interface InsertCellAction {
+    type: ActionType.INSERT_CELL_BEFORE;
+}
+
+interface UpdateCellAction {
+    type: ActionType.UPDATE_CELL;
+}
