cba6892d0d6fd7d53cd9c67f44242418b238fb03
Dustin Hurtt <dustin_hurtt@outlook.com>
2022-09-16 15:30:49 -0400

    continued work on redux and reducers

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -1,8 +1,40 @@
 import { ActionType } from "../action-types";
-import { Action } from "../actions";
+import { Action, UpdateCellAction, DeleteCellAction, MoveCellAction, InsertCellAction, Direction } from "../actions";
+import { CellTypes } from "../cell";
 
-export const updateCell = () => {};
+export const updateCell = (id: string, content: string): UpdateCellAction => {
+    return {
+        type: ActionType.UPDATE_CELL,
+        payload: {
+            id,
+            content
+        },
+    };
+};
 
-export const deleteCell = () => {};
+export const deleteCell = (id: string): DeleteCellAction => {
+    return {
+        type: ActionType.DELETE_CELL,
+        payload: id
+    };
+};
 
-export const moveCell = () => {}
+export const moveCell = (id: string, direction: Direction): MoveCellAction => {
+    return {
+        type: ActionType.MOVE_CELL,
+        payload: {
+            id,
+            direction
+        }
+    };
+};
+
+export const insertCellBefore = (id: string, cellType: CellTypes): InsertCellAction => {
+    return {
+        type: ActionType.INSERT_CELL_BEFORE,
+        payload: {
+            id,
+            type: cellType
+        }
+    };
+};
